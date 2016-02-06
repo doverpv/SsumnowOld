a34281a69cb7d1d74500f3387277226e0c7c1216
hhylu <doverpv@gmail.com>
2016-02-07 05:17:16 +0900

    Initial commit

---
+++ app/src/main/java/com/study/ssumnow/TabFragment1.java
@@ -0,0 +1,45 @@
+package com.study.ssumnow;
+
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.study.cardstack.CardStack;
+
+/**
+ * Created by hhylu on 2016-02-04.
+ */
+public class TabFragment1 extends Fragment {
+    private CardStack mCardStack;
+    private CardsDataAdapter mCardAdapter;
+
+    @Nullable
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+        View view = inflater.inflate(R.layout.tab_fragment_1, container, false);
+
+        mCardStack = (CardStack)view.findViewById(R.id.card_container);
+
+        mCardStack.setContentResource(R.layout.card_content);
+        mCardStack.setStackMargin(20);
+
+        mCardAdapter = new CardsDataAdapter(view.getContext());
+        mCardAdapter.add("test1");
+        mCardAdapter.add("test2");
+        mCardAdapter.add("test3");
+        mCardAdapter.add("test4");
+        mCardAdapter.add("test5");
+
+        mCardStack.setAdapter(mCardAdapter);
+
+        return view;
+    }
+
+    @Override
+    public void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+}
---
+++ app/src/main/java/com/study/ssumnow/TabFragment3.java
@@ -0,0 +1,19 @@
+package com.study.ssumnow;
+
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+/**
+ * Created by hhylu on 2016-02-04.
+ */
+public class TabFragment3 extends Fragment {
+    @Nullable
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+        return inflater.inflate(R.layout.tab_fragment_3, container, false);
+    }
+}
