bfc9cc7b305aabc541b88a289421b7764c4118ae
hhylu <doverpv@gmail.com>
2016-03-03 18:35:49 +0900

    카카오 로그인 버튼 생성 성공

--- app/src/main/java/com/study/ssumnow/MainActivity.java
+++ app/src/main/java/com/study/ssumnow/MainActivity.java
@@ -5,35 +5,23 @@ import android.os.Bundle;
 
 import com.kakao.auth.ISessionCallback;
 import com.kakao.auth.Session;
-import com.kakao.usermgmt.LoginButton;
 import com.kakao.util.exception.KakaoException;
 import com.kakao.util.helper.log.Logger;
 
 //[reference]You want to start a new activity and destroy the previous one? If this is what you need, you can use: startActivity(new Intent(this, myActivity.class).setFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP));
 
 public class MainActivity extends BaseActivity {
-
-    private Session session;
-    private LoginButton loginButton;
     private SessionCallback callback;
 
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-
-        //KakaoSDK.init(new KakaoSDKAdapter());
+        setContentView(R.layout.kakao_login);
 
         callback = new SessionCallback();
-        session = session.getCurrentSession();
-        session.addCallback(callback);
-
-        if (!Session.getCurrentSession().checkAndImplicitOpen()) {
-            setContentView(R.layout.kakao_login);
-        }
-
-        loginButton = (LoginButton)findViewById(R.id.com_kakao_login);
-
+        Session.getCurrentSession().addCallback(callback);
+        Session.getCurrentSession().checkAndImplicitOpen();
     }
 
     @Override
@@ -55,7 +43,7 @@ public class MainActivity extends BaseActivity {
 
         @Override
         public void onSessionOpened() {
-            MainActivity.this.onSessionOpened();
+            redirectSignupActivity();
         }
 
         @Override
@@ -63,14 +51,12 @@ public class MainActivity extends BaseActivity {
             if(exception != null) {
                 Logger.e(exception);
             }
-
-            setContentView(R.layout.kakao_login);
         }
     }
 
-    protected void onSessionOpened() {
-        Intent homeIntent = new Intent(this, HomeActivity.class);
-        startActivity(homeIntent);
+    protected void redirectSignupActivity() {
+        final Intent intent = new Intent(this, HomeActivity.class);
+        startActivity(intent);
         finish();
     }
 }
