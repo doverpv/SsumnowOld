bbb1582cb53d276ab47d819806f8d11c44acab89
hhylu <doverpv@gmail.com>
2016-03-03 14:56:40 +0900

    clear version

--- app/src/main/java/com/study/ssumnow/MainActivity.java
+++ app/src/main/java/com/study/ssumnow/MainActivity.java
@@ -1,6 +1,7 @@
 package com.study.ssumnow;
 
 import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
 
 //[reference]You want to start a new activity and destroy the previous one? If this is what you need, you can use: startActivity(new Intent(this, myActivity.class).setFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP));
@@ -12,5 +13,8 @@ public class MainActivity extends Activity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
 
+        Intent homeIntent = new Intent(this, HomeActivity.class);
+        startActivity(homeIntent);
+        finish();
     }
 }
